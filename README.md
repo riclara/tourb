8e0b212caca783f8daa8db7e330117067168c8b4
Ricardo Lara <riclara@gmail.com>
2019-03-02 12:52:49 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# TECHNICAL EVALUATION BACKEND
+
+>  A ES6 scraper project for evaluation
+## Build Setup
+```sh
+# Run develop environment
+$ git clone https://github.com/riclara/tourb.git # or clone your own fork
+$ cd tourb
+$ npm install
+$ npm run dev
+```
+
+Your app should now be running on [localhost:8000](http://localhost:8000/).
+
+## Deploying to Heroku
+
+```
+$ heroku create
+$ git push heroku master
+$ heroku open
+```
+or
+
+[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+## Documentation
+
+For more information about using Node.js on Heroku, see these Dev Center articles:
+
+- [Getting Started with Node.js on Heroku](https://devcenter.heroku.com/articles/getting-started-with-nodejs)
+- [Heroku Node.js Support](https://devcenter.heroku.com/articles/nodejs-support)
+- [Node.js on Heroku](https://devcenter.heroku.com/categories/nodejs)
+- [Best Practices for Node.js Development](https://devcenter.heroku.com/articles/node-best-practices)
+- [Using WebSockets on Heroku with Node.js](https://devcenter.heroku.com/articles/node-websockets)
---
+++ server/app.js
@@ -0,0 +1,62 @@
+import express from 'express'
+import cors from 'cors'
+import scrapeIt from 'scrape-it'
+
+const app = express()
+const port = 8000
+
+app.use(cors())
+
+function getDetails ({name, url}) {
+  let param = {}
+  param[name] = {
+    listItem: '.element',
+    data: {
+      title: 'h4',
+      desciption: 'p',
+      image: {
+        selector: 'img',
+        attr: 'src'
+      },
+      url: {
+        selector: '.tour_link',
+        attr: 'href'
+      }
+    }
+  }
+  return scrapeIt(url, param)
+}
+
+app.get('/', async (request, response) => {
+  scrapeIt('https://paramotrek.com', {
+    tours: {
+      listItem: '.ppb_tourcat_grid .element',
+      data: {
+        name: 'h3',
+        url: {
+          selector: 'a',
+          attr: 'href'
+        }
+      }
+    }
+  }).then(({ data, resp }) => {
+    let arrPromise = []
+    data.tours.forEach(tour => {
+      arrPromise.push(getDetails(tour))
+    })
+    Promise.all(arrPromise).then(responses => {
+      const res = responses.reduce((curr, val) => {
+        curr.push(val.data)
+        return curr
+      }, [])
+      response.json(res)
+    })
+  }).catch(reason => response.status(500).json({error: reason}))
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('Error on load server: ', err)
+  }
+  console.log(`Server is running and listening on ${port}`)
+})
